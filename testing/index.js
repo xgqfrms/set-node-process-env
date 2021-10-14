995ac170c795bef752e39bfc8c3545a6d64c835a
xgqfrms <xgqfrms@outlook.com>
2021-10-14 12:09:27 +0800

    init

---
+++ testing/index.js
@@ -0,0 +1,20 @@
+"use strict";
+
+/**
+ *
+ * @author xgqfrms
+ * @license MIT
+ * @copyright xgqfrms
+ *
+ * @description testing
+ * @augments
+ * @example
+ * @link
+ *
+ */
+
+const SNPE = require("../src/index");
+
+(() => {
+  SNPE();
+})();
